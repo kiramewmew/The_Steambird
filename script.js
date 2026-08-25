5f114709c7da8de7abe3c9351396a3baf1316fa6
kiramewmew <bakufan94@gmail.com>
2026-08-25 07:46:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// 1. Identificamos las tarjetas por su ID
-const btnReportar = document.querySelector('#btn-reportar');
-const btnManual = document.querySelector('#btn-manual');
-
-// 2. Acción para el botón de Reportar
-btnReportar.addEventListener('click', () => {
-    alert("Iniciando sistema de reporte...");
-    // Aquí podrías llevarlos a otra página: 
-    // window.location.href = "reporte.html";
-});
-
-// 3. Acción para el botón de Manual
-btnManual.addEventListener('click', () => {
-    alert("Abriendo el manual de usuario. Prepárate para 50 páginas de texto aburrido ;)");
-});
 // 1. Seleccionamos el botón y el menú nav
 const botonMenu = document.querySelector('#boton-menu');
 const menuNavegacion = document.querySelector('#navegacion');
